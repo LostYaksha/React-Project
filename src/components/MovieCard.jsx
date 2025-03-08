8b98cf51005a237c5c70fbd5ba1ccf08232e5c51
Rafe <117992417+LostYaksha@users.noreply.github.com>
2025-03-08 11:48:03 +0000

    New upload

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,10 +9,16 @@ function MovieCard({movie}) {
             <img src={movie.url} alt= {movie.title}/>
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onFavoriteClick}>
-                     
-                     </button>
-
+                    🤍
+                </button>
             </div>
-            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
     </div>
 }
+
+
+export default MovieCard;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function NavBar () {
+    return <nav className="navbar">
+        <div className="navbar-brand">
+            <link to="/">Movie App</link>
+        </div>
+        <div className="navbar-menu">
+            <link to="/" className="nav-link">Home</link>
+            <link to="/favorites" className="nav-link">Favorites</link>
+        </div>
+    </nav>
+}
+
+
+export default NavBar;
