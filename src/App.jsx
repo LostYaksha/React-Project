e3c62a5c34c6916c700a2951e4c2c7f0831d3041
Rafe <117992417+LostYaksha@users.noreply.github.com>
2025-03-08 11:47:36 +0000

    new upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import './App.css'
+import './App.css';
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import {Routes, Route} from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-    <Text display ="whats up" />
-    <Text display ="Hello"/>
-    </>
-  )
-}
-
-
-
-function Text({display}) {
-  return (
     <div>
-      <p>{display}</p>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
 
-
-export default App
+export default App;
